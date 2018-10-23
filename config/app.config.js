19a606e365616130c0685e2eba938d855d4a968b
Jerry12138 <448216252@qq.com>
2018-10-23 12:50:45 +0800

    create upload branch

--- config/app.config.js
+++ config/app.config.js
@@ -1,15 +1,15 @@
 /*
- * @Author: Jerry.chenshisong 
- * @Date: 2018-09-27 15:06:58 
+ * @Author: Jerry.chenshisong
+ * @Date: 2018-09-27 15:06:58
  * @Last Modified by: Jerry.chenshisong
- * @Last Modified time: 2018-10-09 15:39:12
+ * @Last Modified time: 2018-10-23 12:31:39
  * 存储七牛cdn地址、空间名、秘钥
  */
 
-let currentPage = 'test'
+const currentPage = 'test'
 
 module.exports = {
-  currentPage:`pages/${currentPage}`,
+  currentPage: `pages/${currentPage}`,
   cdn: {
     host: 'http://pfo0kk2j7.bkt.clouddn.com/',
     bucket: 'hupo-boost-free',
