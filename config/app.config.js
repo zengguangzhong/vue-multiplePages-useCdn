67fbc8f9b92638079402d47b49e2e6cc89b28c0a
Jerry12138 <448216252@qq.com>
2018-10-23 12:51:59 +0800

    add ali-oss config

--- config/app.config.js
+++ config/app.config.js
@@ -2,7 +2,7 @@
  * @Author: Jerry.chenshisong
  * @Date: 2018-09-27 15:06:58
  * @Last Modified by: Jerry.chenshisong
- * @Last Modified time: 2018-10-23 12:31:39
+ * @Last Modified time: 2018-10-23 12:51:01
  * 存储七牛cdn地址、空间名、秘钥
  */
 
@@ -10,10 +10,16 @@ const currentPage = 'test'
 
 module.exports = {
   currentPage: `pages/${currentPage}`,
-  cdn: {
+  qiNiuCdn: {
     host: 'http://pfo0kk2j7.bkt.clouddn.com/',
     bucket: 'hupo-boost-free',
     ak: 'd5tED2TwJGtf3gDprAVaau65giIhZGrBqOWW3OV6',
     sk: 'LmnpMQ1Mg5G0Sdpjc3ZcKxOu_VbJccg3lBnaWV9u'
+  },
+  aLiOss: {
+    accessKeyId: 'LTAIvroOKv2MKWUF',
+    accessKeySecret: 'Yi1Nmxn0nwKw8RqUdOWMIpYLX1tfMH',
+    bucket: 'css-test-cdn',
+    region: 'oss-cn-beijing'
   }
 }
